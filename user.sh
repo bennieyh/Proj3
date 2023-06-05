ae51b637ee7e3c833714e417012daa38f852a6df
bennieyh <bennieyh@outlook.com>
2023-06-05 09:01:12 -0700

    Fixed the user data

--- user.sh
+++ user.sh
@@ -5,10 +5,19 @@ yum install git -y
 yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
+
 cd /var/www/html/
+
+# Clone the repository
 git clone https://github.com/LexJacob/Proj3.git .
-#mv -v /var/www/html/index.html /var/www/html/index.html.backup
+
+# Move the index.html file to the root directory
 mv -v /var/www/html/webpage/index.html /var/www/html/index.html
+
+# Copy all the assets and webpages to the root directory
+cp -r /var/www/html/webpage/* /var/www/html/
+
+# Remove the .git folder
 rm -rf /var/www/html/.git
-#rm -rf /var/www/html/Shrek/
+
 systemctl restart httpd
