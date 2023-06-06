a85a3421d8c8ef189f847bcd08fb1990b1cdfc3d
LexJacob <lpjacob1202@email.campbell.edu>
2023-06-06 15:43:14 -0400

    error for TTL:commented it out. Added 'Rank' for attribute

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,11 +1,15 @@
-resource "aws_dynamodb_table" "score_board" {
+resource "aws_dynamodb_table" "Team-Sequioa" {
   name           = "Team-Sequioa"
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
-  hash_key       = "PlayerName"
+  hash_key       = "Rank"
   range_key      = "HighScore"
 
+  attribute {
+    name = "Rank"
+    type = "S"
+  }
   attribute {
     name = "PlayerName"
     type = "S"
@@ -19,18 +23,22 @@ resource "aws_dynamodb_table" "score_board" {
   stream_enabled      = false
   stream_view_type    = "NEW_IMAGE"
 
-  ttl {
-    attribute_name = "ExpirationTime"
-    enabled        = false
-  }
+  # ttl {
+  #   attribute_name = "ExpirationTime"
+  #   enabled        = false
+  # }
 
   global_secondary_index {
     name               = "HighScoreIndex"
     hash_key           = "HighScore"
-    range_key          = "UserName"
+    range_key          = "PlayerName"
     write_capacity     = 20
     read_capacity      = 20
     projection_type    = "ALL"
     non_key_attributes = []
+  }
+
+  tags = {
+    "Name" = "${var.default_tags.env}"
   }
 }
